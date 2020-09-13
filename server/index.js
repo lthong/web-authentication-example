f99f4df821ea730e73be9373ccdd823abd27a95f
lthong <appyegde58965@gmail.com>
2020-09-13 22:37:38 +0800

    init server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// app setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+
+// server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
