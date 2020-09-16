299ef3d96aa705a24113346a0b46c2f06ce4edf8
lthong <appyegde58965@gmail.com>
2020-09-16 23:00:42 +0800

    connect to the DB

--- server/index.js
+++ server/index.js
@@ -3,7 +3,17 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const mongoose = require('mongoose');
 const router = require('./router');
+
+// DB setup
+mongoose.connect('mongodb://localhost/auth', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+mongoose.connection.on('connected', function () {
+  console.log('connected to db successful!');
+});
 
 // app setup
 app.use(morgan('combined'));
