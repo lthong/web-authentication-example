6bba276a07b4b8d109b155b0db8277f387b13823
lthong <appyegde58965@gmail.com>
2020-09-22 22:42:30 +0800

    add the auth checking by getting the root route

--- server/router.js
+++ server/router.js
@@ -1,8 +1,12 @@
 const auth = require('./controllers/atuh');
+require('./service/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = (app) => {
-  app.get('/', (req, res) => {
-    res.send('Hello Server!');
+  app.get('/', requireAuth, function (req, res) {
+    res.send('Hello server!');
   });
   app.post('/signup', auth.signup);
 };
