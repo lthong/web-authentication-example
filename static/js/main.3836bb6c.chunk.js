72d187ce778e6d5c1b5f83eeab3749c8b712c03e
lthong <appyegde58965@gmail.com>
2020-09-30 17:13:45 +0800

    Updates

---
+++ static/js/main.3836bb6c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpweb-authentication-example-client"]=this["webpackJsonpweb-authentication-example-client"]||[]).push([[0],{3:function(e,t,n){e.exports=n(9)},8:function(e,t,n){},9:function(e,t,n){"use strict";n.r(t);var a=n(0),l=n.n(a),c=n(2),i=n.n(c);n(8);var o=function(){return l.a.createElement("div",null,"Hello world!")};i.a.render(l.a.createElement(l.a.StrictMode,null,l.a.createElement(o,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.3836bb6c.chunk.js.map
