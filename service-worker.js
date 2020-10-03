0d7254f0b077d076a951ccc1e25e43b7acdd7909
lthong <appyegde58965@gmail.com>
2020-10-03 21:33:22 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/web-authentication-example/precache-manifest.eeb6a4ab19dd37ea782d4b01c754e720.js"
+  "/web-authentication-example/precache-manifest.48b73ad2bd1178c0aec56e475c4b989b.js"
 );
 
 self.addEventListener('message', (event) => {
